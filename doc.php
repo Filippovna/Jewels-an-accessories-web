3b7525d52102dbbca675075f3d39dd766778d97c
Patricia <patriciabetancorjimenez@gmail.com>
2019-09-19 14:57:00 +0100

    Jewels and accessories

---
+++ doc.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+
+
+$to = "patriciabetancorjimenez@gmail.com";
+$subject = "New e-mail";
+$body = "You have received the next e-mail from your website. \n\n $name \n\n $email \n\n $text";
+
+mail($to, $subject, $body);
+
+echo "Message sent! click <a href='index.html'> here </a> to go back";
+
+?>
